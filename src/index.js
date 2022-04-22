79fe6c7e7b35aa44e55eab08f17abe3a20038630
Jeevan Prasad <95869206+jeevan243@users.noreply.github.com>
2022-04-22 13:30:52 +0530

    controller updated

--- src/index.js
+++ src/index.js
@@ -3,22 +3,16 @@ const connect = require("./config/db");
 const cors = require('cors')
 const { getUsers, register, login } = require("./controllers/auth.controller");
 const port = process.env.PORT || 5000
-const flatsController = require("./controllers/flat.controller")
-const residentsController = require("./controllers/residents.controller")
-
 
 const app = express();
 app.use(cors())
 
 app.use(express.json());
-app.use("/residents", residentsController)
-app.use("/flats", flatsController)
-
 
 //register
 
 app.post("/register", register);
-app.get ("/userdata", getUsers)
+app.get("/userdata", getUsers)
 app.post("/login", login);
 
 
