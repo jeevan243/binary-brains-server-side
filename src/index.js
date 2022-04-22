5d443ceb5ca7fed3578f566def71cdff2d5e7eb3
Jeevan Prasad <95869206+jeevan243@users.noreply.github.com>
2022-04-22 21:47:00 +0530

    deployement initialized

--- src/index.js
+++ src/index.js
@@ -15,14 +15,15 @@ app.use(cors())
 app.use(express.json());
 
 //register
+app.use("/batch", batchController);
+app.use("/lecture", lectureController);
+app.use("/assignment", assignmentController)
 
 app.post("/register", register);
 app.get("/userdata", getUsers)
 app.post("/login", login);
 
-app.use("/batch", batchController);
-app.use("/lecture", lectureController);
-app.use("/assignment", assignmentController)
+
 
 
 app.listen(port, async (req, res) => {
