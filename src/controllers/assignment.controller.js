31c98e93c8674f021d3ce5c85516020aa713db37
Jeevan Prasad <95869206+jeevan243@users.noreply.github.com>
2022-04-23 20:03:23 +0530

    pagination removed

--- src/controllers/assignment.controller.js
+++ src/controllers/assignment.controller.js
@@ -24,26 +24,38 @@ router.get("/", async (req, res) => {
 })
 
 //get assignments by batch
+// router.get("/:id", async (req, res) => {
+//     try {
+//         let page=req.query.page||1;
+//         let limit=req.query.limit||5;
+//         let offset=(page-1)*limit;
+//         let pages=await Assignment.find({ batch_id: req.params.id }).countDocuments();
+//         let assignments = await Assignment.find({batch_id:req.params.id}).skip(offset).limit(limit).lean().exec();
+//         return res.status(200).send({assignments,pages})
+//     } catch (error) {
+//         return res.status(500).send({ error: error.message })
+//     }
+// })
+
 router.get("/:id", async (req, res) => {
     try {
-        let page=req.query.page||1;
-        let limit=req.query.limit||5;
-        let offset=(page-1)*limit;
-        let pages=await Assignment.find({ batch_id: req.params.id }).countDocuments();
-        let assignments = await Assignment.find({batch_id:req.params.id}).skip(offset).limit(limit).lean().exec();
-        return res.status(200).send({assignments,pages})
+        let assignments = await Assignment.find({ batch_id: req.params.id }).lean().exec();
+        return res.status(200).send(assignments)
     } catch (error) {
         return res.status(500).send({ error: error.message })
     }
 })
 
 
+
+
 router.patch("/:assignment_id", async (req, res) => {
     try {
         let assignments = await Assignment.findById(req.params.assignment_id).lean().exec();
         assignments.students.push(req.body.user_id)
-       await Assignment.findByIdAndUpdate(req.params.assignment_id,assignments);
-        let allassignments = await Assignment.find({batch_id:assignments.batch_id}).lean().exec();
+        await Assignment.findByIdAndUpdate(req.params.assignment_id, assignments);
+
+        let allassignments = await Assignment.find({ batch_id: assignments.batch_id }).lean().exec();
         // console.log(allassignments)
         return res.status(200).send(allassignments);
     } catch (error) {
